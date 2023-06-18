3139bfa6d4178005bcbe597a18c079f8a78e4fe0
hashaska <130322290+hashaska@users.noreply.github.com>
2023-06-19 02:14:29 +0300

    Add files via upload

---
+++ Portal/PortalTeleporter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Player;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public Transform player;
+    public Transform reciever;
+    private bool playerIsOverlapping = false;
+
+    void Update()
+    {
+        if (playerIsOverlapping)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProcut = Vector3.Dot(transform.up, portalToPlayer);
+            //player moves though the portal if its true
+            if (dotProcut < 0f)
+            {
+                //teleport
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
+                playerIsOverlapping = false;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+}
